3d9ccb544e71c38bc6e5f0aeb085731295195020
omarsaadeh-2005 <omar.saadeh@lau.edu>
2026-06-22 16:03:10 +0300

    End of section 12

---
+++ client/src/app/_forms/date-picker/date-picker.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { BsDatepickerConfig, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDatepickerDayDecoratorComponent } from "ngx-bootstrap/datepicker/themes/bs/bs-datepicker-day-decorator.directive";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+@Component({
+  selector: 'app-date-picker',
+  standalone: true,
+  imports: [BsDatepickerDayDecoratorComponent, BrowserAnimationsModule,BsDatepickerModule, ReactiveFormsModule],
+  templateUrl: './date-picker.component.html',
+  styleUrl: './date-picker.component.css'
+})
+export class DatePickerComponent implements ControlValueAccessor{
+  @Input() label='';
+  @Input() maxDate: Date | undefined;
+  bsConfig: Partial<BsDatepickerConfig> |undefined;
+
+    constructor(@Self() public ngControl: NgControl){ 
+      this.ngControl.valueAccessor=this;
+      this.bsConfig={
+        containerClass: "theme-red",
+        dateInputFormat: 'DD MMMM YYYY'
+      }
+    }
+
+  writeValue(obj: any): void {
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  get control(): FormControl{
+    return this.ngControl.control as FormControl
+  }
+  }
+
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NgxSpinnerComponent, NgxSpinnerModule } from "ngx-spinner";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -24,6 +24,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
+import { TextInputComponent } from "./_forms/text-input/text-input.component";
+import { DatePickerComponent } from "./_forms/date-picker/date-picker.component";
 
 @NgModule({
   declarations: [
@@ -47,8 +49,11 @@ import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.compon
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TextInputComponent,
+    DatePickerComponent
 ],
   providers: [
     {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor,multi: true},
